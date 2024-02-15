1849cf20762b6c0b1e9172ea7fe6b77c88a0c18b
José Eduardo S. Carlos <eduardo.carlos.siqueira@gmail.com>
2024-02-15 16:37:45 -0300

    chance connection string

--- src/database/service.js
+++ src/database/service.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const username = process.env.MONGO_USER
 const password = process.env.MONGO_PASSWORD
 
-const url = `mongodb+srv://${username}:${password}@cluster0.3pphasg.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb://${username}:${password}@cluster0.3pphasg.mongodb.net/?retryWrites=true&w=majority`
 
 module.exports = mongoose.connect(url, { connectTimeoutMS: 2 * 60 * 1000 })
